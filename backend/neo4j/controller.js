8df6348a310e6cefafa8980d8887cf1e19677b49
Hetvi Fadadu <hetvi.fadadu@epita.fr>
2026-05-07 17:39:04 +0200

    Added checkFriend controller logic

--- backend/neo4j/controller.js
+++ backend/neo4j/controller.js
@@ -1,5 +1,8 @@
 const { session } = require("./db");
 
+// ----------------------
+// Add Friend
+// ----------------------
 exports.addFriend = async (req, res) => {
     const { user1, user2 } = req.body;
 
@@ -17,6 +20,28 @@ exports.addFriend = async (req, res) => {
         res.json({
             message: "Friend relationship created",
             data: result.records.length
+        });
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+
+exports.checkFriend = async (req, res) => {
+    const { user1, user2 } = req.body;
+
+    try {
+        const query = `
+            MATCH (a:User {id: $user1})-[r:FRIEND]-(b:User {id: $user2})
+            RETURN COUNT(r) AS isFriend
+        `;
+
+        const result = await session.run(query, { user1, user2 });
+
+        const isFriend = result.records[0].get("isFriend").toInt() > 0;
+
+        res.json({
+            friends: isFriend
         });
     } catch (error) {
         res.status(500).json({ error: error.message });

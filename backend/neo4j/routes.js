bc978c29f0a7916f35384f666bbf8af0806805e2
Hetvi Fadadu <hetvi.fadadu@epita.fr>
2026-05-07 18:54:46 +0200

    Feature 4 & 5: Remove Friend + Get All Friends

--- backend/neo4j/routes.js
+++ backend/neo4j/routes.js
@@ -2,10 +2,19 @@ const express = require("express");
 const router = express.Router();
 const neoController = require("./controller");
 
-// Add friend
+// Add Friend
 router.post("/add-friend", neoController.addFriend);
 
-// Check friend relationship
+// Check Friend
 router.post("/check-friend", neoController.checkFriend);
+
+// Mutual Friends
+router.post("/mutual-friends", neoController.mutualFriends);
+
+// Remove Friend
+router.post("/remove-friend", neoController.removeFriend);
+
+// Get All Friends
+router.post("/get-all-friends", neoController.getAllFriends);
 
 module.exports = router;

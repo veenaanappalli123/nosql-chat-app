d39d298f8a3b082c42c5624362e26df609527d66
Hetvi Fadadu <hetvi.fadadu@epita.fr>
2026-05-07 15:43:12 +0200

    Setup Neo4j driver connection

--- backend/neo4j/db.js
+++ backend/neo4j/db.js
@@ -1,1 +1,10 @@
-console.log("Neo4j module ready");
+const neo4j = require("neo4j-driver");
+
+const driver = neo4j.driver(
+    "neo4j://localhost:7687",
+    neo4j.auth.basic("neo4j", "1234")   // <-- use the password you created
+);
+
+const session = driver.session();
+
+module.exports = { driver, session };
